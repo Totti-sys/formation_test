a3b10b9cf8bfe57ac0fcae626ac406e25d055934
Totti <moundtotti@yahoo.fr>
2020-06-23 15:56:46 +0200

    third file

--- src/foramtion_test/FileTosynchroniseWithGitRepo.java
+++ src/foramtion_test/FileTosynchroniseWithGitRepo.java
@@ -1,6 +1,11 @@
 package foramtion_test;
 
 public class FileTosynchroniseWithGitRepo {
+public init _newfield;
 
+//second commit
+public int secondfield;
+
+//Third version -in F2
+public int _thirdfield;
 }
-//commentaire test
